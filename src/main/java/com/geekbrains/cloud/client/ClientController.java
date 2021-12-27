7efd5110609acdaddf6254b749eacd798c886286
Alex <zall99971@gmail.com>
2021-12-28 00:47:40 +0300

    HW 1

--- src/main/java/com/geekbrains/cloud/client/ClientController.java
+++ src/main/java/com/geekbrains/cloud/client/ClientController.java
@@ -6,11 +6,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
 
 public class ClientController implements Initializable {
@@ -19,20 +19,33 @@ public class ClientController implements Initializable {
 
     private DataInputStream is;
     private DataOutputStream os;
+    private File currentDir;
+    private byte[] buf;
 
     public void sendMessage(ActionEvent actionEvent) throws IOException {
-        String message = textField.getText ();
-        os.writeUTF (message);
+        String fileName = textField.getText ();
+        File currentFile = currentDir.toPath ().resolve (fileName).toFile ();
+        os.writeUTF ("#SEND#FILE#");
+        os.writeUTF (fileName);
+        os.writeLong (currentFile.length ());
+        try (FileInputStream is = new FileInputStream (currentFile)) {
+            while (true) {
+                int read = is.read (buf);
+                if (read == -1){
+                    break;
+                }
+                os.write (buf,0,read);
+            }
+        }
         os.flush ();
         textField.clear ();
-        //listView.getItems ().add (message);
     }
 
     private void read() {
         try {
             while (true) {
                 String message = is.readUTF ();
-                Platform.runLater (() -> listView.getItems ().add (message));
+                Platform.runLater (() -> textField.setText (message));
             }
         } catch (Exception e) {
             e.printStackTrace ();
@@ -40,9 +53,36 @@ public class ClientController implements Initializable {
         }
     }
 
+    private void fillCurrentDirFiles() {
+        listView.getItems ().clear ();
+        listView.getItems ().add ("..");
+        listView.getItems ().addAll (currentDir.list ());
+    }
+
+    private void initClickListener() {
+        listView.setOnMouseClicked (e -> {
+            if (e.getClickCount () == 2) {
+                String fileName = listView.getSelectionModel ().getSelectedItem ();
+                System.out.println ("Вабран файл: " + fileName);
+                Path path = currentDir.toPath ().resolve (fileName);
+                if (Files.isDirectory (path)) {
+                    currentDir = path.toFile ();
+                    fillCurrentDirFiles ();
+                    textField.clear ();
+                } else {
+                    textField.setText (fileName);
+                }
+            }
+        });
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            buf = new byte[256];
+            currentDir = new File (System.getProperty ("user.home"));
+            fillCurrentDirFiles ();
+            initClickListener ();
             Socket socket = new Socket ("localhost", 8189);
             is = new DataInputStream (socket.getInputStream ());
             os = new DataOutputStream (socket.getOutputStream ());
